acd70e8d42fcca80c724446fb4318bf9c500903a
vaibhav0704 <vaibhavsaraf.08@gmail.com>
2023-01-14 23:37:34 +0530

    first commit: implemented basic authentication service

---
+++ auth/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { singupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(singupRouter);
+
+app.all('*', async (req, res, next) => {
+  next(new NotFoundError());
+});
+
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log('Listening to port 3000!!!');
+});
