5046685c048ee51cb71ebf9d43c97ec90e6229d4
vaibhav0704 <vaibhavsaraf.08@gmail.com>
2023-01-18 19:00:31 +0530

    implemented basic authentication routes

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,26 +1,22 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator'
-import { RequestValidationError } from '../errors/request-validation-error.';
+import { body } from 'express-validator'
+import jwt from 'jsonwebtoken';
+import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
 
 const router = express.Router();
 
 router.post('/api/users/signup', [
-  body('email')
-    .isEmail()
-    .withMessage('Valid email must be provided'),
-  body('password')
-    .trim()
-    .isLength({ min: 4, max: 20 })
-    .withMessage('Password must be between 4 and 20 characters')
-], 
+    body('email')
+      .isEmail()
+      .withMessage('Valid email must be provided'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20 characters')
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    };
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
@@ -32,6 +28,19 @@ router.post('/api/users/signup', [
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it in session object
+    req.session = {
+      jwt: userJwt
+    };
 
     res.status(201).send(user);
 
