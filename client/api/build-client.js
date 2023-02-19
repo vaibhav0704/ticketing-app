48b91bf8326dfb776f9b7d05f9b8729ccaf77ae2
vaibhav0704 <vaibhavsaraf.08@gmail.com>
2023-02-19 17:31:55 +0530

    client changes

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
     const instance = axios.create({
-      baseURL: 'http://ticket.devaibhav.live',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: {
         ...req.headers,
         accept: 'application/json',
